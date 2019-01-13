5bca8e6b762e2998863fef44033fcdee8d4aa150
mustafa alkan <mustafaalkan64@gmail.com>
2019-01-13 22:17:31 +0300

    Basic Authantication Middleware edited.

--- CustomMiddleware/CustomMiddlewares/BasicAuthanticationMiddleware.cs
+++ CustomMiddleware/CustomMiddlewares/BasicAuthanticationMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
@@ -18,7 +19,7 @@ namespace CustomMiddleware.CustomMiddlewares
         public async Task Invoke(HttpContext context)
         {
             string authHeader = context.Request.Headers["Authorization"];
-            // authorization header format = Mustafaalkan64:Mustafa1234
+            // authorization header format = basic Mustafaalkan64:Mustafa1234
             // Below variables should be get from db
             var userName = ConfigurationManager.AppSetting["Credentials:UserName"];
             var password = ConfigurationManager.AppSetting["Credentials:Password"];
@@ -33,6 +34,12 @@ namespace CustomMiddleware.CustomMiddlewares
                 catch
                 {
                     context.Response.StatusCode = 500;
+                    
+                    ////Format the response from the server
+                    //var jsonString = "{\"foo\":1,\"bar\":false}";
+                    //byte[] data = Encoding.UTF8.GetBytes(jsonString);
+                    //context.Response.ContentType = "application/json";
+                    //await context.Response.Body.WriteAsync(data, 0, data.Length);
                 }
 
                 var credentials = encodedstring.Split(':');
@@ -51,6 +58,20 @@ namespace CustomMiddleware.CustomMiddlewares
                 context.Response.StatusCode = 401;
             }
             await _next(context);
+        }
+        private async Task<string> FormatResponse(HttpResponse response)
+        {
+            //We need to read the response stream from the beginning...
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            //...and copy it into a string
+            string text = await new StreamReader(response.Body).ReadToEndAsync();
+
+            //We need to reset the reader for the response so that the client can read it.
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            //Return the string for the response, including the status code (e.g. 200, 404, 401, etc.)
+            return $"{response.StatusCode}: {text}";
         }
     }
 }
